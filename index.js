3052fd56b14c272d8ff33691feb5cf8b83e4a756
Avani <avani.jadeja@gmail.com>
2023-04-15 19:47:12 -0500

    Add inquirer, fs, and shapes using require()

---
+++ index.js
@@ -0,0 +1,8 @@
+// import Inquirer module using require()
+const inquirer = require("inquirer");
+
+// import File system module using require()
+const fs = require("fs");
+
+// Importing classes from ./lib/shapes directory
+const { Triangle, Circle, Square } = require("./lib/shapes");
